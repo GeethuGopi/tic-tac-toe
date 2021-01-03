e5a442b20b2976f2d11ec59166530d0c120c22dc
Geethu Gopi <geethugopi33@gmail.com>
2021-01-03 14:05:38 +1300

    added some codes

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ const App = ()=> {
   const [winMessage, setWinMessage] = useState("");
 
   const reloadGame = () => {
-    //
+    setIsCross = false;
+    setWinMessage = "";
+    itemArray.fill('empty',0,9);
   };
 
   const checkIsWinner = () =>{
@@ -25,13 +27,35 @@ const App = ()=> {
   };
 
   const changeItem = itemNumber =>{
-    //
+    if(winMessage){
+      return toast (winMessage, { type : "success" });
+    }
+
+    if(itemArray[itemNumber] ==="empty"){
+      itemArray[itemNumber] = isCross ? "cross" : "circle"
+      setIsCross(!isCross)
+    } else {
+      return toast("already filled", {type: "error"} )
+    }
+    checkIsWinner();
   }
   return (
     <Container className = "p-5">
       <ToastContainer position = "bottom-center" />
         <Row>
           <Col md={6} className="offset-md-3">
+            {winMessage ? (
+              <div className= "mb-2 mt-2">
+                <h1 className="text-success text-uppercase text-center">
+                  {winMessage}
+                </h1>
+                <Button color="success" block onClick={reloadGame}>Reload the Game</Button>
+              </div>
+            ):(
+              <h1 className="text-center text-warning">
+                {isCross ? "Cross" : "Circle"} turns
+              </h1>
+            )}
             <div className="grid">
               {itemArray.map((item,index) => (
                   <Card>
